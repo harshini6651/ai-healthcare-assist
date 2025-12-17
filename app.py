46a8d9997af334c2bbeae3169543ee25a9e3b5d5
harshin6651 <lalithasriharshini@gmail.com>
2025-12-17 08:26:31 +0530

    Update app run config for deployment

--- app.py
+++ app.py
@@ -106,4 +106,5 @@ def fetch_faqs(region):
 # Run App
 # ------------------------------
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
